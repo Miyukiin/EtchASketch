7309d7cf37b10179ed1bd3506b17299743f32bf2
Miyukiin <jr.concha99@gmail.com>
2025-06-05 10:45:16 +0800

    Implement creation of grid using user input
    Add JS function removeGrid to remove the grid if inputted value is
    valid. Add validation so user input is always less than or equal to 100.
    Implement buttons and user input box to accept user input to be used in
    recreating the grid.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 
 const gridContainer = document.querySelector("div.grid-container");
+const recreateGridButton = document.querySelector("button.recreate-grid-button");
+const numOfSquaresInputBox = document.querySelector("input.num-of-squares");
 
-createGrid();
+createGrid(16);
 
-function createGrid(){
-    for(let i=0; i<16;i++){
+recreateGridButton.addEventListener("click", (e) => {
+    let userInputValue = parseInt(numOfSquaresInputBox.value)
+    if (userInputValue <= 100){
+        removeGrid();
+        createGrid(userInputValue);
+        return;
+    }
+    alert("Sorry! Max input is 100.")
+})
+
+function createGrid(numOfSquares){
+    for(let i=0; i<numOfSquares;i++){
         let rowOfElements = document.createElement("div");
         rowOfElements.classList = "row-container";
-        for(let i=0; i<16;i++){
+        for(let i=0; i<numOfSquares;i++){
             let gridElement = document.createElement("div");
             gridElement.classList = "grid-element";
             gridElement.addEventListener("mouseover", (e) => {
@@ -17,4 +29,9 @@ function createGrid(){
         }
         gridContainer.appendChild(rowOfElements);
     }
+}
+
+function removeGrid(){
+    const rows = document.querySelectorAll("div.row-container");
+    rows.forEach(element => {element.remove()});
 }
