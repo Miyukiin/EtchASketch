090bd73027001cb5405d860b6b39fcabcdce8797
Miyukiin <jr.concha99@gmail.com>
2025-06-05 09:59:47 +0800

    Implement 16x16 Grid
    Add the relevant logic in scripts.js, the necessary styles in styles.css
    and the elements in index.html for implementing the 16x16 grid.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 
-const gridContainer = document.querySelector("grid-container")
+const gridContainer = document.querySelector("div.grid-container");
+
+createGrid();
+
+function createGrid(){
+    for(let i=0; i<16;i++){
+        let rowOfElements = document.createElement("div");
+        rowOfElements.classList = "row-container";
+        for(let i=0; i<16;i++){
+            let gridElement = document.createElement("div");
+            gridElement.classList = "grid-element";
+            console.log(gridElement);
+            rowOfElements.appendChild(gridElement);
+        }
+        gridContainer.appendChild(rowOfElements);
+    }
+}
